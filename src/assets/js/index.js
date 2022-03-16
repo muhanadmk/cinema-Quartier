583bea626d743bbe75ff5169934bbf95a0936a25
muhanadmk <mouhandmk1@gmail.com>
2022-03-16 10:31:55 +0100

    Injection de msg de err dans  html et la supprimer en focus

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,26 +1,38 @@
 //pour etre sur que tous les elemnets de la page bien telecharger
 const lengthMax = 40;
 const inseteErr = document.querySelector(".form-input");
-
+// pour insérer dedans les inputs de from
+const persone = {
+  nom: "",
+  prenom: "",
+  email: "",
+  dateDeNaissance: Date,
+  address: {
+    nomRue: "",
+    numeroRue: "",
+    cp: "",
+    ville: "",
+  },
+};
 window.addEventListener("DOMContentLoaded", () => {
   main();
 });
 
 function main() {
   const btnSubmit = document.querySelector("#btnSubmit");
-
   // on cliqe on test les input de la from et on les recuperer
   btnSubmit.addEventListener("click", (event) => {
     event.preventDefault();
     const body = document.querySelector("#bodybg");
-     getNom();
-     //getPrenom();
-    //getDate();
-    // getmEmail();
-    // getNomRue();
-     //getNumeroRue();
-    // getCp();
-    //getVille();
+    getNom();
+    getPrenom();
+    getDate();
+    getmEmail();
+    getNomRue();
+    getNumeroRue();
+    getCp();
+    getVille();
+    console.log(persone);
   });
 }
 
@@ -28,8 +40,8 @@ function main() {
 const getNom = () => {
   const nom = document.querySelector("#nom");
   if (
-    (!(nom.value.charAt(0).toUpperCase() == nom.value.charAt(0)) ||
-      !(lengthMax > nom.value.length)) ||
+    !(nom.value.charAt(0).toUpperCase() == nom.value.charAt(0)) ||
+    !(lengthMax > nom.value.length) ||
     !nom.value.trim()
   ) {
     const nomErr = document.querySelector("#nomErr");
@@ -37,69 +49,68 @@ const getNom = () => {
     sepanErr.innerText =
       "le nom doit etre de 2 caractères minimum et 40 maximum";
     nomErr.appendChild(sepanErr);
-    document.querySelector("#nom").className += " fromBorderErr";
+    injecterMsgErr(nom, sepanErr);
     return false;
   } else {
-    return alert(nom.value);
+    persone.nom = nom.value;
   }
 };
 
 // get le nom apers le tester
 const getPrenom = () => {
   const prenom = document.querySelector("#prenom");
-  if ( !(prenom.value.length > 2) || !(lengthMax > prenom.value.length) ||
-    !prenom.value.trim()) {
+  if (
+    !(prenom.value.length > 2) ||
+    !(lengthMax > prenom.value.length) ||
+    !prenom.value.trim()
+  ) {
     const prenomErr = document.querySelector("#prenomErr");
     const sepanErr = document.createElement("span");
     sepanErr.innerText =
-    "le prenom doit etre de 2 caractères minimum et 40 maximum";
+      "le prenom doit etre de 2 caractères minimum et 40 maximum";
     prenomErr.appendChild(sepanErr);
-    document.querySelector("#prenomErr").className += " formErr";
-    return false;
+    injecterMsgErr(prenom, sepanErr);
   } else {
-    return alert(prenom.value);
+    persone.prenom = prenom.value;
   }
 };
 
 // get le nom apers le tester
 
 function getDate() {
-  const dateDeNaissance = document.querySelector("#date_de_naissance").value;
-  const dateDeNaissanceFromat = new Date(dateDeNaissance);
+  const dateDeNaissance = document.querySelector("#date_de_naissance");
+  const dateDeNaissanceFromat = new Date(dateDeNaissance.value);
   const date18YrsAgo = new Date();
   date18YrsAgo.setFullYear(date18YrsAgo.getFullYear() - 18);
-   alert(date18YrsAgo);
+  //alert(date18YrsAgo);
   // alert(dateDeNaissanceFromat);
   if (
     dateDeNaissanceFromat.getFullYear() <= date18YrsAgo.getFullYear() &&
     dateDeNaissanceFromat.getMonth() >= date18YrsAgo.getMonth() &&
     dateDeNaissanceFromat.getDate() >= date18YrsAgo.getDate()
   ) {
-    return console.log(dateDeNaissance);
+    persone.dateDeNaissance = dateDeNaissance.value;
   } else {
     const dateErr = document.querySelector("#dateErr");
     const sepanErr = document.createElement("span");
-    sepanErr.innerText = "vous deovez être majeur au jour de linscription";
+    sepanErr.innerText = "vous devez être majeur au jour de linscription";
     dateErr.appendChild(sepanErr);
-    document.querySelector("#dateErr").className += " formErr";
-    return false;
+    injecterMsgErr(dateDeNaissance, sepanErr);
   }
-  // return alert("non");
 }
 
 // get le nom apers le tester
 
 const getmEmail = () => {
-  const email = document.querySelector("#email").value;
-  if (regExEmail(email)) {
-    return console.log(email);
+  const email = document.querySelector("#email");
+  if (regExEmail(email.value)) {
+    persone.email = email.value;
   } else {
     const emailErr = document.querySelector("#emailErr");
     const sepanErr = document.createElement("span");
     sepanErr.innerText = "vous deovez ecreir un bon fromat de le e-mail";
     emailErr.appendChild(sepanErr);
-    document.querySelector("#emailErr").className += " formErr";
-    return false;
+    injecterMsgErr(email, sepanErr);
   }
 };
 
@@ -110,57 +121,67 @@ const regExEmail = (email) => {
 
 // functions de l'address pour ne pas etre vide our null
 const getNomRue = () => {
-  const nomRue = document.querySelector("#nomRue").value;
-  if (nomRue == null || !nomRue.trim()) {
+  const nomRue = document.querySelector("#nomRue");
+  if (nomRue.value == null || !nomRue.value.trim()) {
     const nomRueErr = document.querySelector("#nomRueErr");
     const sepanErr = document.createElement("span");
-    sepanErr.innerText = "nomRueErr";
+    sepanErr.innerText = "Ce champ est obligatoire, vous devez le remplir.";
     nomRueErr.appendChild(sepanErr);
-    document.querySelector("#nomRueErr").className += " formErr";
-    return false;
+    injecterMsgErr(nomRue, sepanErr);
   } else {
-    return alert("nom rue non valide");
+    persone.address.nomRue = nomRue.value;
   }
 };
 
 const getNumeroRue = () => {
-  const numeroRue = document.querySelector("#numeroRue").value;
-  if (numeroRue == null || !numeroRue.trim()) {
+  const numeroRue = document.querySelector("#numeroRue");
+  if (numeroRue.value == null || !numeroRue.value.trim()) {
     const numeroRueErr = document.querySelector("#numeroRueErr");
     const sepanErr = document.createElement("span");
-    sepanErr.innerText = "numeroRueErr";
+    sepanErr.innerText = "Ce champ est obligatoire, vous devez le remplir.";
     numeroRueErr.appendChild(sepanErr);
-    document.querySelector("#numeroRueErr").className += " formErr";
-    return false;
+    injecterMsgErr(numeroRue, sepanErr);
   } else {
-    return alert(numeroRue);
+    persone.address.numeroRue = numeroRue.value;
   }
 };
 
 const getCp = () => {
-  const cp = document.querySelector("#cp").value;
-  if (cp == null || !cp.trim()) {
+  const cp = document.querySelector("#cp");
+  if (cp.value == null || !cp.value.trim()) {
     const cpErr = document.querySelector("#cpErr");
     const sepanErr = document.createElement("span");
     sepanErr.innerText = "cpErr";
     cpErr.appendChild(sepanErr);
-    document.querySelector("#cpErr").className += " formErr";
-    return false;
+    injecterMsgErr(cp, sepanErr);
   } else {
-    return alert("valide cp");
+    persone.address.cp = cp.value;
   }
 };
 
 const getVille = () => {
-  const ville = document.querySelector("#ville").value;
-  if (ville == null || !ville.trim()) {
+  const ville = document.querySelector("#ville");
+  if (ville.value == null || !ville.value.trim()) {
     const villeErr = document.querySelector("#villeErr");
     const sepanErr = document.createElement("span");
-    sepanErr.innerText = "villeErr";
+    sepanErr.innerText = "Ce champ est obligatoire, vous devez le remplir.";
     villeErr.appendChild(sepanErr);
-    document.querySelector("#villeErr").className += " formErr";
-    return false;
+    injecterMsgErr(ville, sepanErr);
   } else {
-    return alert(" valide ville");
+    persone.address.ville = ville.value;
   }
 };
+
+// Fonction pour afficher une msg de err
+function injecterMsgErr(inputTag, sepanErrElmenet) {
+  inputTag.className += " fromBorderErr";
+  supprimeMsgErr(inputTag, sepanErrElmenet);
+}
+
+//supprimer msg de err lorsque on focus sur l'input
+function supprimeMsgErr(inputTag, sepanErrElmenet) {
+  inputTag.addEventListener("focus", (event) => {
+    inputTag.classList.remove("fromBorderErr");
+    sepanErrElmenet.remove();
+  });
+}
