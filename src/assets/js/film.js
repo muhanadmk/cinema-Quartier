6a5099c8e2233779b3750ea6c3e5bc889095466b
muhanadmk <mouhandmk1@gmail.com>
2022-03-17 10:43:48 +0100

    Paramétrer l'injecte de les éléments html in js, et faire msg err 404

--- src/assets/js/film.js
+++ src/assets/js/film.js
@@ -1,49 +1,45 @@
 window.addEventListener("DOMContentLoaded", () => {
-  getFilm().catch((e) => {
-    console.log("propbleme server vous avez pas get le film " + e.message);
-  });
+  if (getParamTitreFilm() == null) {
+    // injecter l'image et text 404 s'il y a un problème de récupération des données
+    injecteDataDefilm("img", "", '/src/assets/images/err404.gif', "#imgFilm");
+    injecteDataDefilm("h1", "",  "Désole, On ne peut pas traiter votre demande choisissez un autre film.", "#CaracteFilm");
+  }else{
+    getFilm().catch((e) => {
+      console.log("propbleme server vous avez pas get le film " + e.message);
+    });
+  }
 });
 
 const getFilm = async () => {
   const apiFilm = `https://omdbapi.com/?t=${getParamTitreFilm()}&apikey=d821be33`;
-  let response = await fetch(apiFilm);
-  if (!response.ok) {
+  const response = await fetch(apiFilm);
+  if (!response.ok ) {
     throw new Error(response.status);
   } else {
-    let data = await response.json();
-    const insererData = document.querySelector("#CaracteFilm");
-    const insetrPoster = document.querySelector('#imgFilm');
-
-    const poster = document.createElement("img");
-    poster.setAttribute('src',data.Poster);
-    insetrPoster.appendChild(poster)
-
-    const TitleH1 = document.createElement("h1");
-    TitleH1.innerText = data.Title;
-    insererData.appendChild(TitleH1);
-
-
-    const anneeFilme =  document.createElement("p");
-    anneeFilme.innerText = `L'année de sortie : ${data.Year}`;
-    insererData.appendChild(anneeFilme);
-
-   
-    const genre =  document.createElement("p");
-    genre.innerText = `genre : ${data.Genre}`;
-    insererData.appendChild(genre);
-
-    const actors =  document.createElement("p");
-    actors.innerText = `Actors : ${data.Actors}`;
-    insererData.appendChild(actors);
-
-    const resume =  document.createElement("p");
-    resume.innerText = `résumé : ${data.Plot}`;
-    insererData.appendChild(resume);
+    const data = await response.json();
+    injecteDataDefilm("img", "", data.Poster, "#imgFilm");
+    injecteDataDefilm("h1", "", data.Title, "#CaracteFilm");
+    injecteDataDefilm("p", `L'année de sortie : `, data.Year, "#CaracteFilm");
+    injecteDataDefilm("p", `genre : `, data.Genre, "#CaracteFilm");
+    injecteDataDefilm("p", `Actors : `, data.Actors, "#CaracteFilm");
+    injecteDataDefilm("p", `résumé : `, data.Plot, "#CaracteFilm");
   }
 };
 
 // pour récupérer le nom de film de le parm de URL
-function getParamTitreFilm(titreFime) {
-  return new URLSearchParams(window.location.search).get("t");
+function getParamTitreFilm() {
+const getParamTitre = new URLSearchParams(window.location.search).get("t");
+  if (getParamTitre == null) {
+    return null;
+  }
+  return getParamTitre;
 }
 
+// Pour injecter les donnes vias de l'api dans le html
+function injecteDataDefilm(tgeHtml, titreDeDonnees, donnees, positionInPage) {
+  const insererData = document.querySelector(positionInPage);
+  const HtmlTag = document.createElement(tgeHtml);
+  HtmlTag.setAttribute("src", donnees);
+  HtmlTag.innerText = ` ${titreDeDonnees}  ${donnees}`;
+  insererData.appendChild(HtmlTag);
+}
